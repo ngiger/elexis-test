b3a9daadd2583a30da1a1a7d9d0a10cccaabcaa5
Niklaus Giger <niklaus.giger@member.fsf.org>
2011-04-22 22:53:27 +0200

    Added lyx docu für scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,27 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110422192042) do
+ActiveRecord::Schema.define(:version => 20110422202027) do
 
   create_table "product_versions", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "test_beschriebs", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "test_umgebungs", :force => true do |t|
+    t.string   "name"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
