4900da5c03c4ad2364c9b77d3905444aa0a68913
Niklaus Giger <niklaus.giger@member.fsf.org>
2011-04-22 16:47:00 +0200

    Sample App -> Elexis Test gewandelt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElexisTest::Application.config.secret_token = 'a65a219cc2ea5946aca6fc4c4008f153c361868122616282cdfd74e381dcd18487e407199b2f8ff2e6378c4ab4798b15774c87b9667ae497c4be5102866a5e31'
